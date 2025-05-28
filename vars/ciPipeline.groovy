445e7250da9f841d1de234bfcebb5da313671de7
vishakh03 <vishakhvishy2002@gmail.com>
2025-05-28 21:17:35 +0530

    Update ciPipeline.groovy

--- vars/ciPipeline.groovy
+++ vars/ciPipeline.groovy
@@ -1,1 +1,61 @@
-hi
+def call() {
+      pipeline {
+      agent any
+      
+      triggers {
+      // Fires as soon as GitHub sends a push event
+      githubPush()
+      }
+      
+      environment {
+        PATH = "C:\\Windows\\System32;C:\\Program Files\\dotnet;${env.PATH}"
+      }
+      
+      stages {
+        stage('Checkout') {
+            steps {
+                checkout scm
+            }
+        }
+      
+        stage('Restore') {
+            steps {
+                bat 'dotnet restore'
+            }
+        }
+      
+        stage('Build') {
+            steps {
+                bat 'dotnet build --configuration Release --no-restore'
+            }
+        }
+      
+        stage('Test') {
+            steps {
+                bat 'dotnet test --configuration Release --no-build --no-restore'
+            }
+        }
+      
+        stage('Publish') {
+            steps {
+                bat 'dotnet publish --configuration Release --no-build --output publish'
+            }
+        }
+      
+        stage('aetifacts') {
+            steps{
+                archiveArtifacts artifacts: 'publish/**', fingerprint: true
+            }
+        }
+      }
+      
+      post {
+        success {
+            echo '✅ Build, test, publish successful!'
+        }
+        failure {
+            echo '❌ Something went wrong.'
+        }
+      }
+      }
+}
