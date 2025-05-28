370a619b87ef3ad3039e018de2263a7252a930df
vishakh03 <vishakhvishy2002@gmail.com>
2025-05-29 00:27:09 +0530

    Update postAction.groovy

--- vars/postAction.groovy
+++ vars/postAction.groovy
@@ -1,17 +1,17 @@
-def call() {
-  if (env.BRANCH_NAME == 'main') {
-    success {
-            echo '✅ Build, test, publish successful!'
-        }
-        failure {
-            echo '❌ Something went wrong.'
-        }
-  } 
-  else {
-            if (fileExists('smswebapp.tests/smswebapp.tests/TestResults/test-results.xml')) {
-                junit 'smswebapp.tests/smswebapp.tests/TestResults/test-results.xml' 
-            } else {
-                echo 'No test report files found.'
-            }
-  }
-}
+// def call() {
+//   if (env.BRANCH_NAME == 'main') {
+//     success {
+//             echo '✅ Build, test, publish successful!'
+//         }
+//         failure {
+//             echo '❌ Something went wrong.'
+//         }
+//   } 
+//   else {
+//             if (fileExists('smswebapp.tests/smswebapp.tests/TestResults/test-results.xml')) {
+//                 junit 'smswebapp.tests/smswebapp.tests/TestResults/test-results.xml' 
+//             } else {
+//                 echo 'No test report files found.'
+//             }
+//   }
+// }
