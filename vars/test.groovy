0678f7b8ccc26810c2e90250a27ca08f4d803450
vishakh03 <vishakhvishy2002@gmail.com>
2025-05-28 22:25:54 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,3 +1,7 @@
 def call() {
+  if (env.BRANCH_NAME == 'main') {
   bat 'dotnet test --configuration Release --no-build --no-restore'
+  } else {
+   bat 'dotnet test --configuration Release --no-restore --no-build --logger:"junit;LogFilePath=smswebapp.tests/smswebapp.tests/TestResults/test-results.xml"'
+  }
 }
